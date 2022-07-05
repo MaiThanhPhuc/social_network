d869746ca5a75228426f8c086bd026c0fde1f593
MaiThanhPhuc <maithanhphuc7721@gmail.com>
2022-07-05 22:24:13 +0700

    update dasboard Admin

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -1,70 +1,86 @@
+import {useEffect} from "react";
+import {useState} from "react";
+import {AiOutlineUser} from "react-icons/ai";
+import {BsFillFilePostFill} from "react-icons/bs";
+
 const Dashboard = () => {
+  const [users, setUsers] = useState();
+  const [posts, setposts] = useState();
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  useEffect(() => {
+    fetchUsers();
+    fetchPosts();
+  }, []);
+
+  const fetchUsers = async () => {
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${user.access_token}`);
+
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://socialnetwork999.herokuapp.com/admin/api/user?page=0&size=10`,
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => {
+        const payload = JSON.parse(result).data;
+        setUsers(payload.totalElements);
+      })
+      .catch((error) => console.log("error", error));
+  };
+
+  const fetchPosts = async () => {
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${user.access_token}`);
+
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://socialnetwork999.herokuapp.com/admin/api/post?page=0&size=50`,
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => {
+        const payload = JSON.parse(result);
+        console.log(payload);
+        setposts(payload.length);
+      })
+      .catch((error) => console.log("error", error));
+  };
+
   return (
     <>
       <div className="">
         <div className="heading flex justify-center pt-8 ">
           <div className="stats shadow">
-            <div className="stat">
+            <div className="stat bg-red/20">
               <div className="stat-figure text-secondary">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="inline-block w-8 h-8 stroke-current"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  ></path>
-                </svg>
+                <AiOutlineUser size={30} color="#000" />
               </div>
-              <div className="stat-title">Downloads</div>
-              <div className="stat-value">31K</div>
-              <div className="stat-desc">Jan 1st - Feb 1st</div>
+              <div className="stat-title text-black font-medium">
+                Total User
+              </div>
+              <div className="stat-value">{users}</div>
             </div>
 
-            <div className="stat">
+            <div className="stat bg-[#3330E4]/20">
               <div className="stat-figure text-secondary">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="inline-block w-8 h-8 stroke-current"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-                  ></path>
-                </svg>
+                <BsFillFilePostFill size={30} color="#000" />
               </div>
-              <div className="stat-title">New Users</div>
-              <div className="stat-value">4,200</div>
-              <div className="stat-desc">↗︎ 400 (22%)</div>
-            </div>
-
-            <div className="stat">
-              <div className="stat-figure text-secondary">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="inline-block w-8 h-8 stroke-current"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
-                  ></path>
-                </svg>
+              <div className="stat-title text-black font-medium">
+                Total Post
               </div>
-              <div className="stat-title">New Registers</div>
-              <div className="stat-value">1,200</div>
-              <div className="stat-desc">↘︎ 90 (14%)</div>
+              <div className="stat-value">{posts}</div>
             </div>
           </div>
         </div>
